6eb66d4c231ddf00ad59eb5248293fb8f1c0eadc
Emmanuel <52857931+eeTiboah@users.noreply.github.com>
2021-08-07 18:51:26 +0000

    create ssh key

--- main.tf
+++ main.tf
@@ -1,80 +1,120 @@
 provider "aws" {
-    region = "eu-west-2"
+  region = "eu-west-2"
 }
 
 variable "cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "availability_zone" {}
-variable "env_prefix"{}
-variable "my_ip" {
-  
-}
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance_type" {}
+variable "public_key_location" {}
 
 resource "aws_vpc" "app-vpc" {
-    cidr_block = var.cidr_block
-    tags = {
-        Name = "${var.env_prefix}-vpc"
-    }
+  cidr_block = var.cidr_block
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
 }
 
 resource "aws_subnet" "app-subnet-1" {
-    vpc_id = aws_vpc.app-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.availability_zone
-    tags = {
-        Name = "${var.env_prefix}-subnet"
-    }
+  vpc_id            = aws_vpc.app-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+  tags = {
+    Name = "${var.env_prefix}-subnet"
+  }
 }
 
 resource "aws_internet_gateway" "app-igw" {
-    vpc_id = aws_vpc.app-vpc.id
-    tags = {
-        Name = "${var.env_prefix}-igw"
-    }
+  vpc_id = aws_vpc.app-vpc.id
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
 }
 
 resource "aws_route_table" "app-route-table" {
-    vpc_id = aws_vpc.app-vpc.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.app-igw.id
-    }
-    tags = {
-        Name = "${var.env_prefix}-rtb"
-    }
+  vpc_id = aws_vpc.app-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app-igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
 }
 
 resource "aws_route_table_association" "subnet-association-rtb" {
-    subnet_id = aws_subnet.app-subnet-1.id
-    route_table_id = aws_route_table.app-route-table.id 
+  subnet_id      = aws_subnet.app-subnet-1.id
+  route_table_id = aws_route_table.app-route-table.id
 }
 
 resource "aws_security_group" "app-sg" {
-    name = "app-sg"
-    vpc_id = aws_vpc.app-vpc.id
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.my_ip]
-    }
+  name   = "app-sg"
+  vpc_id = aws_vpc.app-vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
 
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"] 
-        prefix_list_ids = []
-    }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
 
-    tags = {
-        Name = "${var.env_prefix}-sg"
-    }
+  tags = {
+    Name = "${var.env_prefix}-sg"
+  }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "aws_sg_id" {
+  value = aws_security_group.app-sg
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "server-key"
+  public_key = file(var.public_key_location)
+
+}
+
+resource "aws_instance" "app-ec2-server" {
+  ami           = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id              = aws_subnet.app-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.app-sg.id]
+  availability_zone      = var.availability_zone
+
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.ssh-key.key_name
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
 }
