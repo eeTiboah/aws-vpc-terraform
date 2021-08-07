ec82c1e08b869b444c19267065a15a2638428105
Emmanuel <52857931+eeTiboah@users.noreply.github.com>
2021-08-07 12:55:05 +0000

    add security group

--- main.tf
+++ main.tf
@@ -6,6 +6,9 @@ variable "cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "availability_zone" {}
 variable "env_prefix"{}
+variable "my_ip" {
+  
+}
 
 resource "aws_vpc" "app-vpc" {
     cidr_block = var.cidr_block
@@ -44,4 +47,34 @@ resource "aws_route_table" "app-route-table" {
 resource "aws_route_table_association" "subnet-association-rtb" {
     subnet_id = aws_subnet.app-subnet-1.id
     route_table_id = aws_route_table.app-route-table.id 
+}
+
+resource "aws_security_group" "app-sg" {
+    name = "app-sg"
+    vpc_id = aws_vpc.app-vpc.id
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"] 
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name = "${var.env_prefix}-sg"
+    }
 }
